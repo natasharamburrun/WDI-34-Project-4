fa81e14d91ed36c06ae11d5a114d35a4583a3fcb
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-07-10 10:53:16 +0100

    yarn seed and added environment

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const port = 4000;
 app.use(express.static(`${__dirname}/public`));
 
 
-
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.listen(port, () => console.log(`Express running on port ${port}`));
