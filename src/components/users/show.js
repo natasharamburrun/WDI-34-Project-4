94788ffa751f6b1d3f665e7bf22b37d879b27ded
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-07-12 11:33:42 +0100

    started the profile page information is now rendering

--- src/components/users/show.js
+++ src/components/users/show.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Auth from '../../lib/Auth';
 
 
@@ -9,15 +9,14 @@ class UsersShow extends React.Component {
   constructor() {
     super();
     this.state = {
+      user: {}
     };
   }
 
-  componentDidMount() {
-    axios({
-      url: `/api/users/${this.props.match.params.id}`,
-      method: 'GET'
-    })
-      .then(res => this.setState({ users: res.data }))
+  componentDidMount(){
+    axios.get(`/api/users/${this.props.match.params.id}`)
+      .then(res => this.setState({ user: res.data }))
+      // .then(res => this.setState({ user: res.data })), () => console.log(this.state.user.username)))
       .catch(err => this.setState({ error: err.message }));
   }
 
@@ -32,35 +31,34 @@ class UsersShow extends React.Component {
 
 
   render() {
-    // if(this.state.error) return <h2 className="title is-2">{this.state.error}</h2>;
-    // if(!this.state.user) return <h2 className="title is-2">Loading...</h2>;
+    if(this.state.error) return <h2 className="title is-2">{this.state.error}</h2>;
+    if(!this.state.user) return <h2 className="title is-2">Loading...</h2>;
     return (
       <section className="user-show">
-        <h1>what is going on!!!</h1>
-        {/* <section className="hero is-primary">
+        <section className="hero">
           <div className="hero-body">
             <figure className="media-content-image">
               <p className="image is-128x128">
-                <img src="{this.state.user.image}" />
+                <img src="{this.state.users.image}" />
               </p>
             </figure>
             <article className="media">
               <div className="media-content">
                 <div className="content">
-                  <p>
-                    <h2 className="title">{this.state.user.username}</h2>
-                    <h2 className="title">{this.state.user.email}</h2>
-                    <h2 className="title">{this.state.user.bio}</h2>
-                  </p>
+
+                  <h2 className="title">{this.state.user.username}</h2>
+                  <h2 className="title">{this.state.user.email}</h2>
+                  <h2 className="title">{this.state.user.bio}</h2>
+
                 </div>
               </div>
             </article>
             <div className="media-right">
-              <Link className="button" to={`/cupcakes/${this.state.user._id}/edit`}>Edit</Link>
+              <Link className="button" to={`/users/${this.state.user._id}/edit`}>Edit</Link>
               <button className="button is-danger" onClick={this.handleDelete}>Delete</button>
             </div>
           </div>
-        </section> */}
+        </section>
       </section>
     );
   }
